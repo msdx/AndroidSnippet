5a70b93aebe63085cc40804ce6e8672ad8312fa8
msdx <645079761@qq.com>
2015-09-08 13:22:49 +0800

    Add auto manage feature into AppManager.

--- library/src/main/java/com/githang/android/snippet/app/AppManager.java
+++ library/src/main/java/com/githang/android/snippet/app/AppManager.java
@@ -1,9 +1,13 @@
 package com.githang.android.snippet.app;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -13,16 +17,69 @@ import java.util.Stack;
  * 应用程序Activity管理类：用于Activity管理和应用程序退出
  *
  * @author HaohangHuang (msdx.android@qq.com)
- * @since 0.1
  * @version 0.4
+ * @since 0.1
  */
 public class AppManager {
     private static final String LOG_TAG = AppManager.class.getSimpleName();
     private static Stack<Activity> activityStack = new Stack<Activity>();
+    private static boolean autoManage = false;
+
+    /**
+     * 自动管理Activity，需要 android api 不低于14。
+     *
+     * @param application
+     */
+    @TargetApi(14)
+    public static void enableAutoManage(Application application) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            throw new UnsupportedOperationException("The android os version of this device is unsupported.");
+        }
+        if (!autoManage) {
+            application.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+                @Override
+                public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                    add(activity);
+                }
+
+                @Override
+                public void onActivityStarted(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityResumed(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityPaused(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivityStopped(Activity activity) {
+
+                }
+
+                @Override
+                public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+                }
+
+                @Override
+                public void onActivityDestroyed(Activity activity) {
+                    remove(activity);
+                }
+            });
+            autoManage = true;
+        }
+    }
 
     /**
      * 添加Activity到堆栈
-     * @param activity    要添加进入的Activity
+     *
+     * @param activity 要添加进入的Activity
      */
     public static void add(Activity activity) {
         activityStack.push(activity);
@@ -30,6 +87,7 @@ public class AppManager {
 
     /**
      * 从堆栈中移除指定的Activity
+     *
      * @param activity 要移除的Activity
      */
     public static void remove(Activity activity) {
@@ -48,7 +106,9 @@ public class AppManager {
      */
     public static void finishCurrentActivity() {
         Activity activity = activityStack.pop();
-        activity.finish();
+        if (activity != null) {
+            activity.finish();
+        }
     }
 
     /**
@@ -72,7 +132,7 @@ public class AppManager {
                 activities.add(activity);
             }
         }
-        for(Activity activity: activities) {
+        for (Activity activity : activities) {
             activityStack.remove(activity);
         }
     }
@@ -103,11 +163,13 @@ public class AppManager {
             Log.w(LOG_TAG, e);
         }
     }
+
     /**
      * 启动界面
+     *
      * @param context Context对象
-     * @param clazz Activity的Class对象
-     * @param flag Intent标志
+     * @param clazz   Activity的Class对象
+     * @param flag    Intent标志
      */
     public static void start(Context context, Class<? extends Activity> clazz, int flag) {
         Intent intent = new Intent(context, clazz);
