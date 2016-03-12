6f8b93846c2eef0c459d9a54c8daa6509a951dbf
msdx <645079761@qq.com>
2016-03-12 11:30:43 +0800

    Add sample about listview choice mode.

--- app/src/main/java/com/githang/android/snippet/demo/adapter/ChoiceListActivity.java
+++ app/src/main/java/com/githang/android/snippet/demo/adapter/ChoiceListActivity.java
@@ -2,7 +2,10 @@ package com.githang.android.snippet.demo.adapter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.githang.android.snippet.adapter.ChoiceListAdapter;
@@ -12,22 +15,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * FIXME
+ * 单选，多选，全选，反选的例子。
  *
  * @author Geek_Soledad (msdx.android@qq.com)
  */
 public class ChoiceListActivity extends Activity {
+    private RadioGroup mChoiceMode;
+    private View mChoiceAll;
+    private View mReverseChoice;
+    private View mButtons;
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_choice_list);
+        mChoiceMode = (RadioGroup) findViewById(R.id.choice_mode);
+        mButtons = findViewById(R.id.buttons);
+        mChoiceAll = findViewById(R.id.all);
+        mReverseChoice = findViewById(R.id.reverse);
+        mListView = (ListView) findViewById(android.R.id.list);
 
         List<String> data = new ArrayList<>();
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             data.add("test" + i);
         }
-        ListView listView = new ListView(this);
-        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        ChoiceListAdapter adapter = new ChoiceListAdapter<String>(this, R.layout.item_single_choice,
+        final ChoiceListAdapter adapter = new ChoiceListAdapter<String>(this, R.layout.item_single_choice,
                 data, R.id.checkedView) {
             @Override
             protected void holdView(ChoiceLayout view) {
@@ -40,7 +53,43 @@ public class ChoiceListActivity extends Activity {
                 text.setText(data);
             }
         };
-        listView.setAdapter(adapter);
-        this.setContentView(listView);
+        mListView.setAdapter(adapter);
+
+        mChoiceMode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.single_choice) {
+                    mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+                    mListView.setAdapter(adapter);
+                    mButtons.setVisibility(View.INVISIBLE);
+                } else {
+                    mListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                    mButtons.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        mChoiceAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int size = adapter.getCount();
+                for (int i = 0; i < size; i++) {
+                    mListView.setItemChecked(i, true);
+                }
+            }
+        });
+
+        mReverseChoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray checkedItems = mListView.getCheckedItemPositions();
+                final int size = adapter.getCount();
+                for (int i = 0; i < size; i++) {
+                    mListView.setItemChecked(i, !checkedItems.get(i));
+                }
+            }
+        });
+
+        mChoiceMode.check(R.id.single_choice);
     }
 }
