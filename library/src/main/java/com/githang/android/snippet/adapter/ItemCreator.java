6cb7ea4615f4f0e049e01339c5c0c8534353c16e
msdx <645079761@qq.com>
2017-12-15 10:55:09 +0800

    Add DrawableTint

--- library/src/main/java/com/githang/android/snippet/adapter/ItemCreator.java
+++ library/src/main/java/com/githang/android/snippet/adapter/ItemCreator.java
@@ -2,8 +2,19 @@ package com.githang.android.snippet.adapter;
 
 import android.view.ViewGroup;
 
+/**
+ * @author msdx.android@qq.com
+ * @param <T> 数据
+ * @param <H> ItemHolder 子类
+ */
 public interface ItemCreator<T, H extends ItemHolder> {
 
+    /**
+     * 创建指定位置的 ViewHolder
+     * @param position 指定位置
+     * @param parent item 父布局
+     * @return ViewHolder
+     */
     H createHolder(int position, ViewGroup parent);
 
     /**
@@ -11,6 +22,7 @@ public interface ItemCreator<T, H extends ItemHolder> {
      *
      * @param position 在列表中的位置
      * @param holder   该位置对应的视图
+     * @param data     对应位置的数据
      */
     void bindData(int position, H holder, T data);
 }
---
+++ library/src/main/java/com/githang/android/snippet/graphics/DrawableTint.java
@@ -0,0 +1,45 @@
+package com.githang.android.snippet.graphics;
+
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.graphics.drawable.TintAwareDrawable;
+
+/**
+ * @author Geek_Soledad (msdx.android@qq.com)
+ * @since 0.6.4
+ */
+public class DrawableTint {
+
+    /**
+     * 对指定 Drawable 进行着色
+     *
+     * @param drawable 需要着色的 drawable
+     * @param tint     状态颜色列表
+     * @return 着色后的 drawable
+     */
+    public static Drawable tint(Drawable drawable, ColorStateList tint) {
+        drawable = DrawableCompat.wrap(drawable);
+        DrawableTint.setTintMode(drawable, PorterDuff.Mode.MULTIPLY);
+        drawable = drawable.mutate();
+        DrawableCompat.setTintList(drawable, tint);
+        return drawable;
+    }
+
+    /**
+     * 这里使用的是TintAwareDrawable的setTintMode方法
+     *
+     * @param drawable 需要着色的drawable
+     * @param mode     PorterDuff.Mode
+     */
+    @SuppressLint("RestrictedApi")
+    private static void setTintMode(Drawable drawable, PorterDuff.Mode mode) {
+        if (drawable instanceof TintAwareDrawable) {
+            ((TintAwareDrawable) drawable).setTintMode(mode);
+        } else {
+            DrawableCompat.setTintMode(drawable, mode);
+        }
+    }
+}
