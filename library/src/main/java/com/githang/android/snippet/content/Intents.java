24747fa663e711c7473e83cf9d9ecbb8ce1cc29b
msdx <645079761@qq.com>
2017-12-15 10:33:26 +0800

    Add app setting intent

--- library/src/main/java/com/githang/android/snippet/content/Intents.java
+++ library/src/main/java/com/githang/android/snippet/content/Intents.java
@@ -1,13 +1,16 @@
 package com.githang.android.snippet.content;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 
 /**
  * Intent 工具类。
  *
  * @author msdx.android@qq.com
+ * @version 2017-12-15 0.6.4
  * @since 2015-06-18
  */
 public class Intents {
@@ -18,16 +21,29 @@ public class Intents {
      * @param phoneNumber 要拨打的号码
      * @return 打电话的Intent
      */
-    public static final Intent call(String phoneNumber) {
-        Uri uri = Uri.parse("tel:" + phoneNumber);
+    public static Intent call(String phoneNumber) {
+        final Uri uri = Uri.parse("tel:" + phoneNumber);
         return new Intent(Intent.ACTION_DIAL, uri);
     }
 
     /**
      * 定位设置的Intent
+     *
      * @return 返回定位设置的Intent
      */
-    public static final Intent locationSetting() {
+    public static Intent locationSetting() {
         return new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+    }
+
+    /**
+     * 获取 App 设置的 Intent
+     *
+     * @param context 要跳转的 app
+     * @return App 设置的 Intent
+     * @since 0.6.4
+     */
+    public static Intent appSetting(@NonNull Context context) {
+        return new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                .setData(Uri.fromParts("package", context.getPackageName(), null));
     }
 }
