8ab54d96c87e7b5a43fd868ee21289d1c97abf06
msdx <645079761@qq.com>
2018-04-11 09:47:41 +0800

    Support tint drawable with the specified mode and color

--- library/src/main/java/com/githang/android/snippet/graphics/DrawableTint.java
+++ library/src/main/java/com/githang/android/snippet/graphics/DrawableTint.java
@@ -1,23 +1,29 @@
 package com.githang.android.snippet.graphics;
 
-import android.annotation.SuppressLint;
 import android.content.res.ColorStateList;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorInt;
+import android.support.annotation.NonNull;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v4.graphics.drawable.TintAwareDrawable;
 
 /**
  * @author Geek_Soledad (msdx.android@qq.com)
- * @since 0.6.4
+ * @since 0.6.5
  */
 public class DrawableTint {
 
-    public static Drawable tint(Drawable drawable, @ColorInt int tint) {
-        drawable = DrawableCompat.wrap(drawable);
-        DrawableTint.setTintMode(drawable, PorterDuff.Mode.MULTIPLY);
-        drawable = drawable.mutate();
+    /**
+     * Tint the drawable with the special mode and color
+     *
+     * @param drawable The drawable to be tint
+     * @param mode     tint mode
+     * @param tint     tint color
+     * @return The tint drawable
+     */
+    public static Drawable tint(Drawable drawable, PorterDuff.Mode mode, @ColorInt int tint) {
+        drawable = setTintModeAndMutate(drawable, mode);
         DrawableCompat.setTint(drawable, tint);
         return drawable;
     }
@@ -26,29 +32,26 @@ public class DrawableTint {
      * 对指定 Drawable 进行着色
      *
      * @param drawable 需要着色的 drawable
+     * @param mode     The tint mode
      * @param tint     状态颜色列表
      * @return 着色后的 drawable
      */
-    public static Drawable tint(Drawable drawable, ColorStateList tint) {
-        drawable = DrawableCompat.wrap(drawable);
-        DrawableTint.setTintMode(drawable, PorterDuff.Mode.MULTIPLY);
-        drawable = drawable.mutate();
+    public static Drawable tint(Drawable drawable, PorterDuff.Mode mode, ColorStateList tint) {
+        drawable = setTintModeAndMutate(drawable, mode);
         DrawableCompat.setTintList(drawable, tint);
         return drawable;
     }
 
-    /**
-     * 这里使用的是TintAwareDrawable的setTintMode方法
-     *
-     * @param drawable 需要着色的drawable
-     * @param mode     PorterDuff.Mode
-     */
-    @SuppressLint("RestrictedApi")
-    private static void setTintMode(Drawable drawable, PorterDuff.Mode mode) {
+    @NonNull
+    private static Drawable setTintModeAndMutate(Drawable drawable, PorterDuff.Mode mode) {
+        drawable = DrawableCompat.wrap(drawable);
         if (drawable instanceof TintAwareDrawable) {
             ((TintAwareDrawable) drawable).setTintMode(mode);
         } else {
             DrawableCompat.setTintMode(drawable, mode);
         }
+        drawable = drawable.mutate();
+        return drawable;
     }
+
 }
